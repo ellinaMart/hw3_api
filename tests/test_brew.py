e06c20071ed9949845b05e92acb869c8560d47a1
ellinaMart <ellina.martyn@gmail.com>
2021-07-28 03:48:33 +0300

    test api1

--- tests/test_brew.py
+++ tests/test_brew.py
@@ -11,27 +11,27 @@ def test_api(base_url2):
 
 @pytest.mark.parametrize('city', ['San Diego', 'Alameda'])
 def test_api_city(base_url2, city):
-    resp = requests.get(base_url2, data={'by_city': city})
+    resp = requests.get(base_url2, params={'by_city': city})
     assert resp.status_code == 200
     for i in range(len(resp.json())):
         assert resp.json()[i]['city'] == city
 
 @pytest.mark.parametrize('name, expected', testdata)
 def test_api_name(base_url2, name, expected):
-    resp = requests.get(base_url2, data={'by_name': name})
+    resp = requests.get(base_url2, params={'by_name': name})
     assert resp.status_code == 200
 
 @pytest.mark.parametrize('state', ['Ohio','New York','New Mexico'])
 def test_api_state(base_url2, state):
-    resp = requests.get(base_url2, data={'by_state': state})
+    resp = requests.get(base_url2, params={'by_state': state})
     assert resp.status_code == 200
     for i in range(len(resp.json())):
         assert resp.json()[i]['state'] == state
 
 @pytest.mark.parametrize('postal', ['44107', '44107-4020'])
 def test_api_postal(base_url2, postal):
-    resp = requests.get(base_url2, data={'by_postal': postal})
+    resp = requests.get(base_url2, params={'by_postal': postal})
     assert resp.status_code == 200
     for i in range(len(resp.json())):
-        assert resp.json()[i]['postal_code'] == postal
+        assert postal in resp.json()[i]['postal_code']
 
--- tests/test_param.py
+++ tests/test_param.py
@@ -4,3 +4,5 @@ import requests
 def test_url_status(base_url4, resp_status_code):
     response = requests.get(base_url4)
     assert response.status_code == int(resp_status_code)
+
+# пример запуска pytest tests/test_param.py --url4=https://mail.ru --status_code=200
